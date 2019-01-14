f4f0c228b0368caa4f62b0be9ca797d79add0cf5
dokim33 <45578846+dokim33@users.noreply.github.com>
2019-01-14 18:57:38 -0500

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require_once('header.php');
-    if(!isset($_GET['id'])){
-    require_once "home.php";
-  } else {
-   require_once "{$_GET['id']}.php";
-  }
-
-    require_once('footer.php');
